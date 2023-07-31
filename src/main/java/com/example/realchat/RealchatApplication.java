26ee699293edaf914607e497e895de4dfc007a40
melvinsibichen <melvinsibichen1020@gmail.com>
2023-07-31 21:32:55 +0530

    first commit

---
+++ src/main/java/com/example/realchat/RealchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.realchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealchatApplication.class, args);
+	}
+
+}
